914bbc7c08bb3c20560da228bc85b66e0bc3454a
svaria <shaanvaria@MacBook-Air.local>
2015-06-07 15:31:22 -0700

    need to change to use mongoose

--- HMSREST/app.js
+++ HMSREST/app.js
@@ -4,14 +4,17 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var passport = require('passport')
   , BasicStrategy = require('passport-http').BasicStrategy;
 var GoogleStrategy = require('passport-google').Strategy
-
+var db = require('mongoskin').db('localhost:27017/HMS');
+console.log(db.collection('users').find());
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,11 +26,45 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({ secret: '12312901', 
+                resave: true, 
+                saveUninitialized: true
+                }));
 app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+/* Basic Http authentication strategy set up */
+passport.use(new BasicStrategy(
+  function(username, password, done) {
+    db.collection('users').findOne({username:username}, function (err,user) {
+      if(err) { return done(err); }
+      if(!user) { return done(null, false); }
+      if(!user.validPassword(password)) { return done(null,false) }
+      return done(null,user);
+    });
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done){
+  done(null, obj);
+});
+
+app.get('/login',
+        passport.authenticate('basic'),
+        function(req,res){
+          res.json(req.user);
+        });
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -78,19 +115,5 @@ app.get('/auth/google/return',
            passport.authenticate('google', { successRedirect: '/',
                                  failureRedirect: '/login' }));
 /* end passport setup */
-
-/* Basic Http authentication strategy set up */
-passport.use(new BasicStrategy(
-  function(username, password, done) {
-    User.findOne({username:username}, function (err,user) {
-      if(err) { return done(err); }
-      if(!user) { return done(null, false); }
-      if(!user.validPassword(password)) { return done(null,false) }
-      return done(null,user);
-    });
-}));
-
-
-
 
 module.exports = app;
